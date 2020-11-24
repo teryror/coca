502c51590aa5383609ba45cbd8dd06b3529479e4
Tristan Dannenberg <dtristan@hotmail.de>
2020-11-24 01:28:10 +0100

    First bounded List implementation with typed index

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
+#![cfg_attr(feature = "nightly", feature(min_const_generics))]
 
 pub mod arena;
+pub mod list;
 
 pub use crate::arena::{Arena, Box};
+pub use crate::list::{ArrayList, List, TiArrayList};
