b719bd476203da79bbd4c7180b5230523ae4834b
Tristan Dannenberg <dtristan@hotmail.de>
2020-12-22 20:59:01 +0100

    Implement more methods on Deque

--- src/deque.rs
+++ src/deque.rs
@@ -121,6 +121,30 @@ where
         unsafe { Some(self.buf.storage_mut()[index].as_mut_ptr().as_mut().unwrap()) }
     }
 
+    /// Returns a reference to the front element, or [`None`] if the `Deque` is empty.
+    #[inline]
+    pub fn front(&self) -> Option<&E> {
+        self.get(I::from_usize(0))
+    }
+
+    /// Returns a mutable reference to the front element, or [`None`] if the `Deque` is empty.
+    #[inline]
+    pub fn front_mut(&mut self) -> Option<&mut E> {
+        self.get_mut(I::from_usize(0))
+    }
+
+    /// Returns a reference to the back element, or [`None`] if the `Deque` is empty.
+    #[inline]
+    pub fn back(&self) -> Option<&E> {
+        self.get(I::from_usize(self.len() - 1))
+    }
+
+    /// Returns a mutable reference to teh back element, or [`None`] if the `Deque is empty.
+    #[inline]
+    pub fn back_mut(&mut self) -> Option<&mut E> {
+        self.get_mut(I::from_usize(self.len() - 1))
+    }
+
     /// Removes the first element and returns it, or [`None`] if the `Deque` is empty.
     ///
     /// # Examples
@@ -249,6 +273,46 @@ where
         }
     }
 
+    /// Shortens the `Deque`, keeping the first `len` elements and dropping the rest.
+    ///
+    /// If `len` is greater than the deque's current length, this has no effect.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut backing_region = [core::mem::MaybeUninit::<i32>::uninit(); 4];
+    /// let mut deque = coca::SliceDeque::<i32>::from(&mut backing_region[..]);
+    /// deque.push_back(5);
+    /// deque.push_back(10);
+    /// deque.push_back(15);
+    /// assert_eq!(deque.as_slices(), (&[5, 10, 15][..], &[][..]));
+    /// deque.truncate(1);
+    /// assert_eq!(deque.as_slices(), (&[5][..], &[][..]));
+    /// ```
+    pub fn truncate(&mut self, len: I) {
+        let new_len = len.into_usize();
+        let old_len = self.len();
+
+        if new_len >= old_len {
+            return;
+        }
+
+        for i in new_len..old_len {
+            let idx = i % self.capacity();
+            let ptr = self.buf.storage_mut().as_mut_ptr() as *mut E;
+            unsafe {
+                ptr.add(idx).drop_in_place();
+            }
+        }
+
+        self.len = len;
+    }
+
+    /// Clears the `Deque`, dropping all values.
+    pub fn clear(&mut self) {
+        self.truncate(I::from_usize(0));
+        self.front = I::from_usize(0);
+    }
+
     /// Swaps the elements at indices `i` and `j`.
     ///
     /// `i` and `j` may be equal.
@@ -276,6 +340,238 @@ where
             .physical_index(j)
             .expect("index `j` is out of bounds in `swap`");
         self.buf.storage_mut().swap(i, j);
+    }
+
+    /// Removes an element from anywhere in the `Deque` and returns it, replacing
+    /// it with the front element.
+    ///
+    /// This does not preserve ordering, but is O(1).
+    ///
+    /// Returns [`None`] if `index` is out of bounds.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut backing_region = [core::mem::MaybeUninit::<i32>::uninit(); 4];
+    /// let mut deque = coca::SliceDeque::<i32>::from(&mut backing_region[..]);
+    /// deque.push_back(1);
+    /// deque.push_back(2);
+    /// deque.push_back(3);
+    /// assert_eq!(deque.swap_remove_front(2), Some(3));
+    /// assert_eq!(deque.make_contiguous(), &[2, 1]);
+    /// ```
+    pub fn swap_remove_front(&mut self, index: I) -> Option<E> {
+        let index = self.physical_index(index)?;
+        let front = self.front.into_usize();
+
+        unsafe {
+            let last = self.buf.storage()[front].as_ptr().read();
+            let hole = self.buf.storage_mut()[index].as_mut_ptr();
+            self.len = I::from_usize(self.len() - 1);
+            self.front = I::from_usize((front + 1) % self.capacity());
+            Some(hole.replace(last))
+        }
+    }
+
+    /// Removes an element from anywhere in the `Deque` and returns it,
+    /// replacing it with the back element.
+    ///
+    /// This does not preserve ordering, but is O(1).
+    ///
+    /// Returns [`None`] if `index` is out of bounds.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut backing_region = [core::mem::MaybeUninit::<i32>::uninit(); 4];
+    /// let mut deque = coca::SliceDeque::<i32>::from(&mut backing_region[..]);
+    /// deque.push_back(1);
+    /// deque.push_back(2);
+    /// deque.push_back(3);
+    /// assert_eq!(deque.swap_remove_back(0), Some(1));
+    /// assert_eq!(deque.make_contiguous(), &[3, 2]);
+    /// ```
+    pub fn swap_remove_back(&mut self, index: I) -> Option<E> {
+        let index = self.physical_index(index)?;
+        let back = (self.front.into_usize() + self.len() - 1) % self.capacity();
+
+        unsafe {
+            let last = self.buf.storage()[back].as_ptr().read();
+            let hole = self.buf.storage_mut()[index].as_mut_ptr();
+            self.len = I::from_usize(self.len() - 1);
+            Some(hole.replace(last))
+        }
+    }
+
+    /// Inserts an element at `index` within the `Deque`, returning `Err(value)`
+    /// if it is full.
+    ///
+    /// Whichever end is closer to the insertion point will be moved to make
+    /// room, and all elements in between will be shifted to new positions.
+    ///
+    /// The element at index 0 is the front of the queue.
+    ///
+    /// # Panics
+    /// Panics if `index` is greater than the deque's length.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut backing_region = [core::mem::MaybeUninit::<char>::uninit(); 4];
+    /// let mut deque = coca::SliceDeque::<char>::from(&mut backing_region[..]);
+    /// deque.push_back('a');
+    /// deque.push_back('b');
+    /// deque.push_back('c');
+    /// assert_eq!(deque.make_contiguous(), &['a', 'b', 'c'][..]);
+    ///
+    /// assert!(deque.try_insert(1, 'd').is_ok());
+    /// assert_eq!(deque.make_contiguous(), &['a', 'd', 'b', 'c'][..]);
+    /// ```
+    pub fn try_insert(&mut self, index: I, value: E) -> Result<(), E> {
+        if self.is_full() {
+            return Err(value);
+        }
+
+        let index = index.into_usize();
+        if index > self.len() {
+            panic!("index out of bounds in `try_insert`");
+        }
+
+        let cap = self.capacity();
+        let front = self.front.into_usize();
+        let back = front + self.len();
+        let contiguous = back < self.capacity();
+
+        let distance_to_front = index;
+        let distance_to_back = self.len() - index;
+        let move_front = distance_to_front < distance_to_back;
+
+        let new_front = if move_front {
+            (front + self.capacity() - 1) % self.capacity()
+        } else {
+            front
+        };
+
+        let index_wrapped = new_front + index >= self.capacity();
+
+        match (contiguous, move_front, index_wrapped) {
+            (true, true, _) => {
+                // storage is contiguous, insertion point is in lower half
+                // -> shift all elements before the insertion point to the left by one
+                if distance_to_front > 0 {
+                    let dst = self.buf.storage_mut()[new_front].as_mut_ptr();
+                    let src = self.buf.storage()[front].as_ptr();
+                    unsafe { core::ptr::copy(src, dst, 1) };
+
+                    let dst = self.buf.storage_mut()[front].as_mut_ptr();
+                    let src = self.buf.storage()[front + 1].as_ptr();
+                    unsafe { core::ptr::copy(src, dst, distance_to_front - 1) };
+                }
+
+                let ptr = self.buf.storage_mut()[(new_front + index) % cap].as_mut_ptr();
+                unsafe { ptr.write(value) };
+            }
+            (true, false, _) => {
+                // storage is contiguous, insertion point is in upper half
+                // -> shift all elements after the insertion point to the right by one
+                let physical_index = front + index;
+                let src = self.buf.storage()[physical_index].as_ptr();
+                let dst = self.buf.storage_mut()[physical_index + 1].as_mut_ptr();
+                unsafe { core::ptr::copy(src, dst, distance_to_back) };
+                let ptr = self.buf.storage_mut()[physical_index].as_mut_ptr();
+                unsafe { ptr.write(value) };
+            }
+            (false, true, false) => {
+                // storage is not contiguous, insertion point is in lower half and does not wrap
+                // -> shift all elements before the insertion point to the left by one
+                let ptr = self.buf.storage_mut()[new_front].as_mut_ptr();
+                unsafe {
+                    ptr.write(value);
+                }
+                self.buf.storage_mut()[new_front..].rotate_left(1);
+            }
+            (false, false, true) => {
+                // storage is not contiguous, insertion point is in upper half and does wrap
+                // -> shift all elements after the insertion point to the right by one
+                let physical_index = (front + index) % self.capacity();
+                let ptr = self.buf.storage_mut()[physical_index + distance_to_back].as_mut_ptr();
+                unsafe {
+                    ptr.write(value);
+                }
+                self.buf.storage_mut()[physical_index..=physical_index + distance_to_back]
+                    .rotate_right(1);
+            }
+            (false, true, true) => {
+                // storage is not contiguous, insertion point is in the lower half, but the index wraps
+                // -> shift all elements before the insertion point to the left by one, accounting for wrap
+                let physical_index = (new_front + index) % self.capacity();
+                let ptr = self.buf.storage_mut()[0].as_mut_ptr();
+                let tmp = unsafe { ptr.replace(value) };
+                self.buf.storage_mut()[..=physical_index].rotate_left(1);
+
+                let ptr = self.buf.storage_mut()[new_front].as_mut_ptr();
+                unsafe {
+                    ptr.write(tmp);
+                }
+                self.buf.storage_mut()[new_front..].rotate_left(1);
+            }
+            (false, false, false) => {
+                // storage is not contiguous, insertion point is in the upper half, but the index doesn't wrap
+                // -> shift all elements after the insertion point to the right by one, accounting for wrap
+                let physical_index = (front + index) % cap;
+                let ptr = self.buf.storage_mut()[cap - 1].as_mut_ptr();
+                let tmp = unsafe { ptr.replace(value) };
+                self.buf.storage_mut()[physical_index..].rotate_right(1);
+
+                let back = back % cap;
+                let ptr = self.buf.storage_mut()[back].as_mut_ptr();
+                unsafe {
+                    ptr.write(tmp);
+                }
+                self.buf.storage_mut()[..back + 1].rotate_right(1);
+            }
+        }
+
+        self.len = I::from_usize(self.len() + 1);
+        self.front = I::from_usize(new_front);
+        Ok(())
+    }
+
+    /// Inserts an element at `index` within the `Deque`.
+    ///
+    /// Whichever end is closer to the insertion point will be moved to make
+    /// room, and all elements in between will be shifted to new positions.
+    ///
+    /// The element at index 0 is the front of the queue.
+    ///
+    /// # Panics
+    /// Panics if `index` is greater than the deque's length, or if the deque
+    /// is already at capacity. See [`try_insert`](Deque::try_insert) for a
+    /// checked version.
+    pub fn insert(&mut self, index: I, value: E) {
+        if self.try_insert(index, value).is_err() {
+            panic!("deque is already at capacity");
+        }
+    }
+
+    /// Places an element at position `index` within the `Deque`, returning the
+    /// element previously stored there.
+    ///
+    /// # Panics
+    /// Panics if `index` is greater than or equal to the deque's length.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut backing_region = [core::mem::MaybeUninit::<i32>::uninit(); 4];
+    /// let mut deque = coca::SliceDeque::<i32>::from(&mut backing_region[..]);
+    /// deque.push_back(1);
+    /// deque.push_back(2);
+    /// deque.push_back(4);
+    /// assert_eq!(deque.replace(2, 3), 4);
+    /// assert_eq!(deque.make_contiguous(), &[1, 2, 3][..]);
+    /// ```
+    pub fn replace(&mut self, index: I, value: E) -> E {
+        let index = self
+            .physical_index(index)
+            .expect("index out of bounds in `replace`");
+        unsafe { self.buf.storage_mut()[index].as_mut_ptr().replace(value) }
     }
 
     /// Returns a pair of slices which contain, in order, the contents of the `Deque`.
@@ -380,5 +676,46 @@ where
             let ptr = self.buf.storage_mut().as_mut_ptr() as *mut E;
             unsafe { core::slice::from_raw_parts_mut(ptr, self.len()) }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn all_insertion_cases() {
+        let mut backing_region = [core::mem::MaybeUninit::<i32>::uninit(); 8];
+        let mut deque = SliceDeque::<i32>::from(&mut backing_region[..]);
+
+        deque.try_insert(0, 1).unwrap();
+        assert_eq!(deque.as_slices(), (&[1][..], &[][..]));
+
+        deque.try_insert(0, 2).unwrap();
+        assert_eq!(deque.as_slices(), (&[2][..], &[1][..]));
+
+        deque.try_insert(1, 3).unwrap();
+        assert_eq!(deque.as_slices(), (&[2][..], &[3, 1][..]));
+
+        deque.try_insert(1, 4).unwrap();
+        assert_eq!(deque.as_slices(), (&[2, 4][..], &[3, 1][..]));
+
+        deque.push_back(5);
+        deque.push_back(6);
+        deque.push_back(7);
+        assert_eq!(deque.as_slices(), (&[2, 4][..], &[3, 1, 5, 6, 7][..]));
+
+        deque.try_insert(3, 8).unwrap();
+        assert_eq!(deque.as_slices(), (&[2, 4, 3][..], &[8, 1, 5, 6, 7][..]));
+
+        deque.pop_back();
+        deque.pop_back();
+        deque.pop_back();
+        deque.push_front(5);
+        deque.push_front(6);
+        assert_eq!(deque.as_slices(), (&[6, 5, 2, 4, 3][..], &[8, 1][..]));
+
+        deque.try_insert(4, 7).unwrap();
+        assert_eq!(deque.as_slices(), (&[6, 5, 2, 4, 7][..], &[3, 8, 1][..]));
     }
 }
