7d621e90ddefb221e6d6ae9beccc4d27893ea232
Tristan Dannenberg <dtristan@hotmail.de>
2021-12-13 16:04:09 +0100

    Implement ListMap::entry API

--- src/collections/list_map.rs
+++ src/collections/list_map.rs
@@ -2,10 +2,13 @@
 
 use core::alloc::{Layout, LayoutError};
 use core::borrow::Borrow;
+use core::fmt::Debug;
 use core::marker::PhantomData;
 use core::mem::MaybeUninit;
 
 use crate::storage::{Capacity, LayoutSpec, Storage};
+
+use self::Entry::*;
 
 /// The [`LayoutSpec`] for a [`ListMap`].
 pub struct ListMapLayout<K, V>(PhantomData<(K, V)>);
@@ -148,26 +151,32 @@ impl<K, V, S: Storage<ListMapLayout<K, V>>, I: Capacity> ListMap<K, V, S, I> {
     /// # Panics
     /// Panics if the map is full and does not contain the given key.
     /// See [`try_entry`](ListMap::try_entry) for a checked version that never panics.
-    pub fn entry(&mut self, key: K) /* -> Entry<???> */
+    pub fn entry(&mut self, key: K) -> Entry<'_, K, V, S, I>
     where
         K: Eq
     {
-        todo!()
+        self.try_entry(key).ok().expect("map is already at capacity")
     }
 
     /// Gets the given key's corresponding [`Entry`] in the map for in-place manipulation.
     /// 
-    /// Returns [`Err`] if the map is full and does not contain the given key.
+    /// Returns [`Err(key)`] if the map is full and does not contain the given key.
     /// 
     /// # Examples
     /// ```
     /// todo!()
     /// ```
-    pub fn try_entry(&mut self, key: K) /* -> Result<Entry<???>, ()> */
+    pub fn try_entry(&mut self, key: K) -> Result<Entry<'_, K, V, S, I>, K>
     where
         K: Eq
     {
-        todo!()
+        if let Some((idx, _)) = self.lookup(&key) {
+            Ok(Occupied(OccupiedEntry { key, idx, map: self, }))
+        } else if self.is_full() {
+            Err(key)
+        } else {
+            Ok(Vacant(VacantEntry { key, map: self }))
+        }
     }
 
     #[inline(always)]
@@ -406,3 +415,323 @@ unsafe impl<K, V, const N: usize> Storage<ListMapLayout<K, V>> for InlineStorage
         N
     }
 }
+
+/// A view into an occupied entry in a [`ListMap`]. It is part of the [`Entry`] enum.
+pub struct OccupiedEntry<'a, K, V, S: Storage<ListMapLayout<K, V>>, I: Capacity> {
+    key: K,
+    idx: usize,
+    map: &'a mut ListMap<K, V, S, I>,
+}
+
+impl<'a, K, V, S: Storage<ListMapLayout<K, V>>, I: Capacity> OccupiedEntry<'a, K, V, S, I> {
+    /// Gets a reference to the key used to create the entry.
+    pub fn key(&self) -> &K {
+        &self.key
+    }
+
+    /// Take the ownership of the key and value from the map.
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn remove_entry(self) -> (K, V) {
+        unsafe {
+            let base_ptr = self.map.buf.get_mut_ptr();
+
+            let keys_ptr = base_ptr.cast::<K>();
+            let k = keys_ptr.add(self.idx).read();
+
+            let values_ptr = base_ptr.add(self.map.values_offset()).cast::<V>();
+            let v = values_ptr.add(self.idx).read();
+
+            let new_len = self.map.len() - 1;
+            if self.idx != new_len {
+                core::ptr::copy_nonoverlapping(keys_ptr.add(new_len), keys_ptr.add(self.idx), 1);
+                core::ptr::copy_nonoverlapping(values_ptr.add(new_len), values_ptr.add(self.idx), 1);
+            }
+
+            self.map.len = I::from_usize(new_len);
+            (k , v)
+        }
+    }
+
+    /// Gets a reference to the value in the entry.
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn get(&self) -> &V {
+        unsafe {
+            let base_ptr = self.map.buf.get_ptr();
+            let values_ptr = base_ptr.add(self.map.values_offset()).cast::<V>();
+            values_ptr.add(self.idx).as_ref().unwrap()
+        }
+    }
+
+    /// Gets a mutable reference to the value in the entry.
+    /// 
+    /// If you need a reference to the value which may outlive the `Entry`,
+    /// see [`into_mut`](OccupiedEntry::into_mut).
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn get_mut(&mut self) -> &mut V {
+        unsafe {
+            let base_ptr = self.map.buf.get_mut_ptr();
+            let values_ptr = base_ptr.add(self.map.values_offset()).cast::<V>();
+            values_ptr.add(self.idx).as_mut().unwrap()
+        }
+    }
+
+    /// Converts the `OccupiedEntry` into a mutable reference to the value
+    /// in the entry with a lifetime bound to the map itself.
+    /// 
+    /// If you need multiple references to the `OccupiedEntry`,
+    /// see [`get_mut`](OccupiedEntry::get_mut).
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn into_mut(self) -> &'a mut V {
+        unsafe {
+            let base_ptr = self.map.buf.get_mut_ptr();
+            let values_ptr = base_ptr.add(self.map.values_offset()).cast::<V>();
+            values_ptr.add(self.idx).as_mut().unwrap()
+        }
+    }
+
+    /// Sets the value of the entry, and returns the entry's old value.
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn insert(&mut self, value: V) -> V {
+        unsafe {
+            let base_ptr = self.map.buf.get_mut_ptr();
+            let values_ptr = base_ptr.add(self.map.values_offset()).cast::<V>();
+            values_ptr.add(self.idx).replace(value)
+        }
+    }
+
+    /// Takes the value out of the entry, and returns it.
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn remove(self) -> V {
+        unsafe {
+            let base_ptr = self.map.buf.get_mut_ptr();
+
+            let values_ptr = base_ptr.add(self.map.values_offset()).cast::<V>();
+            let value = values_ptr.add(self.idx).read();
+            
+            let new_len = self.map.len() - 1;
+            if self.idx != new_len {
+                let keys_ptr = base_ptr.cast::<K>();
+                core::ptr::copy_nonoverlapping(keys_ptr.add(new_len), keys_ptr.add(self.idx), 1);
+                core::ptr::copy_nonoverlapping(values_ptr.add(new_len), values_ptr.add(self.idx), 1);
+            }
+
+            self.map.len = I::from_usize(new_len);
+            value
+        }
+    }
+
+    /// Replaces the entry, returning the old key-value pair. The new key in the map will be the key used to create the entry.
+    pub fn replace_entry(self, value: V) -> (K, V) {
+        unsafe {
+            let base_ptr = self.map.buf.get_mut_ptr();
+
+            let keys_ptr = base_ptr.cast::<K>();
+            let k = keys_ptr.add(self.idx).replace(self.key);
+
+            let values_ptr = base_ptr.add(self.map.values_offset()).cast::<V>();
+            let v = values_ptr.add(self.idx).replace(value);
+
+            (k, v)
+        }
+    }
+
+    /// Replaces the key in the map with the one used to create the entry.
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn replace_key(self) -> K {
+        unsafe {
+            let keys_ptr = self.map.buf.get_mut_ptr().cast::<K>();
+            keys_ptr.add(self.idx).replace(self.key)
+        }
+    }
+}
+
+impl<K: Debug, V: Debug, S: Storage<ListMapLayout<K, V>>, I: Capacity> Debug for OccupiedEntry<'_, K, V, S, I> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("OccupiedEntry")
+            .field("key", &self.key)
+            .field("value", self.get())
+            .finish()
+    }
+}
+
+/// A view into a vacant entry in a [`ListMap`]. It is part of the [`Entry`] enum.
+pub struct VacantEntry<'a, K, V, S: Storage<ListMapLayout<K, V>>, I: Capacity> {
+    key: K,
+    map: &'a mut ListMap<K, V, S, I>,
+}
+
+impl<'a, K, V, S: Storage<ListMapLayout<K, V>>, I: Capacity> VacantEntry<'a, K, V, S, I> {
+    /// Gets a reference to the key that would be used when inserting through the `VacantEntry`.
+    pub fn key(&self) -> &K {
+        &self.key
+    }
+
+    /// Take ownership of the key.
+    pub fn into_key(self) -> K {
+        self.key
+    }
+
+    /// Sets the value of the entry with the `VacantEntry`'s key, and returns a mutable reference to it.
+    pub fn insert(self, value: V) -> &'a mut V {
+        unsafe {
+            let len = self.map.len();
+            let base_ptr = self.map.buf.get_mut_ptr();
+
+            let keys_ptr = base_ptr.cast::<K>();
+            keys_ptr.add(len).write(self.key);
+
+            let values_ptr = base_ptr.add(self.map.values_offset()).cast::<V>();
+            let v_ptr = values_ptr.add(len);
+            v_ptr.write(value);
+
+            self.map.len = I::from_usize(len + 1);
+            v_ptr.as_mut().unwrap()
+        }
+    }
+}
+
+impl<K: Debug, V: Debug, S: Storage<ListMapLayout<K, V>>, I: Capacity> Debug for VacantEntry<'_, K, V, S, I> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_tuple("VacantEntry").field(&self.key).finish()
+    }
+}
+
+/// A view into a single entry in a map, which may be either vacant or occupied.
+/// 
+/// This `enum` is constructed from the [`try_entry`](ListMap::try_entry) method on [`ListMap`].
+pub enum Entry<'a, K, V, S: Storage<ListMapLayout<K, V>>, I: Capacity> {
+    /// An occupied entry.
+    Occupied(OccupiedEntry<'a, K, V, S, I>),
+    /// A vacant entry.
+    Vacant(VacantEntry<'a, K, V, S, I>),
+}
+
+impl<'a, K, V, S: Storage<ListMapLayout<K, V>>, I: Capacity> Entry<'a, K, V, S, I> {
+    /// Ensures a value is in the entry by inserting the `default` if empty,
+    /// and returns a mutable reference to the value in the entry.
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn or_insert(self, default: V) -> &'a mut V {
+        match self {
+            Occupied(entry) => entry.into_mut(),
+            Vacant(entry) => entry.insert(default),
+        }
+    }
+
+    /// Ensures a value is in the entry by inserting the result of the `default`
+    /// function if empty, and returns a mutable reference to the value in the entry.
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn or_insert_with<F: FnOnce() -> V>(self, default: F) -> &'a mut V {
+        match self {
+            Occupied(entry) => entry.into_mut(),
+            Vacant(entry) => entry.insert(default()),
+        }
+    }
+
+    /// Ensures a value is in the entry by inserting, if empty, the result of the default function.
+    /// This method allows for generating key-derived values for insertion by providing the default
+    /// function a reference to the key that was moved during the `.entry(key)` method call.
+    ///
+    /// The reference to the moved key is provided so that cloning or copying the key is
+    /// unnecessary, unlike with `.or_insert_with(|| ... )`.
+    ///
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn or_insert_with_key<F: FnOnce(&K) -> V>(self, default: F) -> &'a mut V {
+        match self {
+            Occupied(entry) => entry.into_mut(),
+            Vacant(entry) => {
+                let value = default(entry.key());
+                entry.insert(value)
+            }
+        }
+    }
+
+    /// Returns a reference to the key used to create the entry.
+    pub fn key(&self) -> &K {
+        match *self {
+            Occupied(ref entry) => entry.key(),
+            Vacant(ref entry) => entry.key(),
+        }
+    }
+
+    /// Provides in-place mutable access to an occupied entry before any
+    /// potential inserts into the map.
+    ///
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn and_modify<F: FnOnce(&mut V)>(self, f: F) -> Self {
+        match self {
+            Occupied(mut entry) => {
+                f(entry.get_mut());
+                Occupied(entry)
+            },
+            Vacant(entry) => Vacant(entry),
+        }
+    }
+}
+
+impl<'a, K, V: Default, S: Storage<ListMapLayout<K, V>>, I: Capacity> Entry<'a, K, V, S, I> {
+    /// Ensures a value is in the entry by inserting the default value if empty,
+    /// and returns a mutable reference to the value in the entry.
+    /// 
+    /// # Examples
+    /// ```
+    /// todo!()
+    /// ```
+    pub fn or_default(self) -> &'a mut V {
+        match self {
+            Occupied(entry) => entry.into_mut(),
+            Vacant(entry) => entry.insert(V::default())
+        }
+    }
+}
+
+impl<K: Debug, V: Debug, S: Storage<ListMapLayout<K, V>>, I: Capacity> Debug for Entry<'_, K, V, S, I> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match *self {
+            Vacant(ref v) => f.debug_tuple("Entry").field(v).finish(),
+            Occupied(ref o) => f.debug_tuple("Entry").field(o).finish(),
+        }
+    }
+}
+
